5bd0ddd9d16d4e3752ebdb79731d064e0eae62ac
shahedc <shahedc@gmail.com>
2018-06-21 13:33:17 -0400

    Web App for Batch Pool interaction
    Web App for Batch Pool interaction, initial version using ASP .NET Core 2.1 MVC web app template

---
+++ function-batch/BatchPoolWebApp/BatchPoolWebApp/Models/PoolModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatchPoolWebApp.Models
+{
+    public enum AllocationState
+    {
+        Steady = 0,
+        Resizing = 1,
+        Stopping = 2
+    }
+
+    public class PoolModel
+    {
+        public string PoolId { get; set; }
+        public AllocationState AllocationState { get; set; }
+    }
+}
